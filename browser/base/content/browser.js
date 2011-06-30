7fbefeadd654bb4df6df3175196116b44b37634d
Margaret Leibovic <margaret.leibovic@gmail.com>
2011-06-29 17:01:54 -0700

    Bug 455553 - New Tab Feature

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -175,6 +175,7 @@ XPCOMUtils.defineLazyGetter(this, "PopupNotifications", function () {
 
 let gInitialPages = [
   "about:blank",
+  "about:newtab",
   "about:privatebrowsing",
   "about:sessionrestore"
 ];
@@ -2183,7 +2184,7 @@ function BrowserOpenTab()
                       "chrome,all,dialog=no", "about:blank");
     return;
   }
-  gBrowser.loadOneTab("about:blank", {inBackground: false});
+  gBrowser.loadOneTab("about:newtab", {inBackground: false});
   focusAndSelectUrlBar();
 }
 
