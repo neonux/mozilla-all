463b86a15907324bb2faf265e497ab1df256ad29
Margaret Leibovic <margaret.leibovic@gmail.com>
2011-08-17 17:31:31 -0700

    backout 0b6d05fbb0ed

--- browser/components/search/test/browser_426329.js
+++ browser/components/search/test/browser_426329.js
@@ -3,8 +3,8 @@ function test() {
 
   var searchBar = BrowserSearch.searchBar;
   var searchButton = document.getAnonymousElementByAttribute(searchBar,
-                     "anonid", "search-go-label");
-  ok(searchButton, "got search-go-label");
+                     "anonid", "search-go-button");
+  ok(searchButton, "got search-go-button");
 
   searchBar.value = "test";
 
