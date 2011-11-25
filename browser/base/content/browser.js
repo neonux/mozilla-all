c6733b42d321f76321834ecbf97d4c12fbb08517
Tim Taubert <tim.taubert@gmx.de>
2011-11-25 03:17:06 +0100

    Bug 455553 - New Tab Page feature

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -173,6 +173,12 @@ XPCOMUtils.defineLazyGetter(this, "PopupNotifications", function () {
   }
 });
 
+XPCOMUtils.defineLazyGetter(this, "NewTabUtils", function() {
+  let tmp = {};
+  Cu.import("resource:///modules/NewTabUtils.jsm", tmp);
+  return tmp.NewTabUtils;
+});
+
 XPCOMUtils.defineLazyGetter(this, "InspectorUI", function() {
   let tmp = {};
   Cu.import("resource:///modules/inspector.jsm", tmp);
@@ -181,6 +187,7 @@ XPCOMUtils.defineLazyGetter(this, "InspectorUI", function() {
 
 let gInitialPages = [
   "about:blank",
+  "about:newtab",
   "about:privatebrowsing",
   "about:sessionrestore"
 ];
@@ -1698,6 +1705,7 @@ function delayedStartup(isLoadingBlank, mustLoadSidebar) {
   gSyncUI.init();
 #endif
 
+  NewTabUtils.init();
   TabView.init();
 
   // Enable Inspector?
@@ -2203,10 +2211,10 @@ function BrowserOpenTab()
   if (!gBrowser) {
     // If there are no open browser windows, open a new one
     window.openDialog("chrome://browser/content/", "_blank",
-                      "chrome,all,dialog=no", "about:blank");
+                      "chrome,all,dialog=no", "about:newtab");
     return;
   }
-  gBrowser.loadOneTab("about:blank", {inBackground: false});
+  gBrowser.loadOneTab("about:newtab", {inBackground: false});
   focusAndSelectUrlBar();
 }
 
