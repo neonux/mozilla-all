d68489b3f4c4e177f5f32d5561e2ff9f6fb7a683
Jared Wein <jwein@mozilla.com>
2012-01-30 17:41:53 +0100

    Backed out changeset 678c2cd357c9 (bug 663772)

--- browser/components/nsBrowserGlue.js
+++ browser/components/nsBrowserGlue.js
@@ -27,7 +27,6 @@
 #   Ehsan Akhgari <ehsan.akhgari@gmail.com>
 #   Nils Maier <maierman@web.de>
 #   Robert Strong <robert.bugzilla@gmail.com>
-#   Paolo Amadini <http://www.amadzone.org/>
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or
@@ -1101,7 +1100,7 @@ BrowserGlue.prototype = {
   },
 
   _migrateUI: function BG__migrateUI() {
-    const UI_VERSION = 6;
+    const UI_VERSION = 5;
     const BROWSER_DOCURL = "chrome://browser/content/browser.xul#";
     let currentUIVersion = 0;
     try {
