467165f8098ba95638b50d6aa2c0f1130f6cb202
Frank Yan <fyan@mozilla.com>
2012-03-31 02:08:54 -0700

    Backed out changeset a7a30dee62cc

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6830,16 +6830,6 @@ function warnAboutClosingWindow() {
 #endif
 }
 
-function OnWindowDragEnter(event) {
-  let draggedTab = event.dataTransfer.mozGetDataAt(TAB_DROP_TYPE, 0);
-  if (draggedTab && draggedTab.parentNode) {
-    let panel = draggedTab.parentNode._tabDragPanel;
-    panel._noHide = true;
-    window.focus();
-    delete panel._noHide;
-  }
-}
-
 var MailIntegration = {
   sendLinkForWindow: function (aWindow) {
     this.sendMessage(aWindow.location.href,
