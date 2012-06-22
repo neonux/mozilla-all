60d0c603a55b604380071f9a04c24b58003e9646
Ali Juma <ajuma@mozilla.com>
2012-06-22 15:36:55 -0400

    Import OpenGL context management code (and related code) from LayerManagerOGL to CompositorOGL.

--- gfx/layers/opengl/CompositorOGL.cpp
+++ gfx/layers/opengl/CompositorOGL.cpp
@@ -5,9 +5,270 @@
 
 #include "CompositorOGL.h"
 #include "TextureOGL.h"
+#include "mozilla/Preferences.h"
+
+#include "GLContextProvider.h"
+
+#include "nsIServiceManager.h"
+#include "nsIConsoleService.h"
+
+#include "gfxCrashReporterUtils.h"
 
 namespace mozilla {
 namespace layers {
+
+using namespace mozilla::gl;
+
+CompositorOGL::CompositorOGL(nsIWidget *aWidget, int aSurfaceWidth,
+                             int aSurfaceHeight, bool aIsRenderingToEGLSurface)
+  : mWidget(aWidget)
+  , mSurfaceSize(aSurfaceWidth, aSurfaceHeight)
+  , mBackBufferFBO(0)
+  , mBackBufferTexture(0)
+  , mHasBGRA(0)
+  , mIsRenderingToEGLSurface(aIsRenderingToEGLSurface)
+  , mDestroyed(false)
+{
+}
+
+already_AddRefed<mozilla::gl::GLContext>
+CompositorOGL::CreateContext()
+{
+  nsRefPtr<GLContext> context;
+
+#ifdef XP_WIN
+  if (PR_GetEnv("MOZ_LAYERS_PREFER_EGL")) {
+    printf_stderr("Trying GL layers...\n");
+    context = gl::GLContextProviderEGL::CreateForWindow(mWidget);
+  }
+#endif
+
+  if (!context)
+    context = gl::GLContextProvider::CreateForWindow(mWidget);
+
+  if (!context) {
+    NS_WARNING("Failed to create LayerManagerOGL context");
+  }
+  return context.forget();
+}
+
+void
+CompositorOGL::AddPrograms(ShaderProgramType aType)
+{
+  for (PRUint32 maskType = MaskNone; maskType < NumMaskTypes; ++maskType) {
+    if (ProgramProfileOGL::ProgramExists(aType, static_cast<MaskType>(maskType))) {
+      mPrograms[aType].mVariations[maskType] = new ShaderProgramOGL(this->gl(),
+        ProgramProfileOGL::GetProfileFor(aType, static_cast<MaskType>(maskType)));
+    } else {
+      mPrograms[aType].mVariations[maskType] = nsnull;
+    }
+  }
+}
+
+void
+CompositorOGL::Destroy()
+{
+  mDestroyed = true;
+
+  // TODO: Cleanup resources here.
+}
+
+bool
+CompositorOGL::Initialize(bool force, nsRefPtr<GLContext> aContext)
+{
+  ScopedGfxFeatureReporter reporter("GL Layers", force);
+
+  // Do not allow double initialization
+  NS_ABORT_IF_FALSE(mGLContext == nsnull, "Don't reinitialize CompositorOGL");
+
+  if (aContext) {
+    mGLContext = aContext;
+  } else {
+    mGLContext = CreateContext();
+  }
+
+#ifdef MOZ_WIDGET_ANDROID
+  if (!mGLContext)
+    NS_RUNTIMEABORT("We need a context on Android");
+#endif
+
+  if (!mGLContext)
+    return false;
+
+  mGLContext = aContext;
+  mGLContext->SetFlipped(true);
+
+  MakeCurrent();
+
+  mHasBGRA =
+    mGLContext->IsExtensionSupported(gl::GLContext::EXT_texture_format_BGRA8888) ||
+    mGLContext->IsExtensionSupported(gl::GLContext::EXT_bgra);
+
+  mGLContext->fBlendFuncSeparate(LOCAL_GL_ONE, LOCAL_GL_ONE_MINUS_SRC_ALPHA,
+                                 LOCAL_GL_ONE, LOCAL_GL_ONE);
+  mGLContext->fEnable(LOCAL_GL_BLEND);
+
+  mPrograms.AppendElements(NumProgramTypes);
+  for (int type = 0; type < NumProgramTypes; ++type) {
+    AddPrograms(static_cast<ShaderProgramType>(type));
+  }
+
+  // initialise a common shader to check that we can actually compile a shader
+  if (!mPrograms[gl::RGBALayerProgramType].mVariations[MaskNone]->Initialize()) {
+    return false;
+  }
+
+
+  mGLContext->fGenFramebuffers(1, &mBackBufferFBO);
+
+  if (mGLContext->WorkAroundDriverBugs()) {
+
+    /**
+    * We'll test the ability here to bind NPOT textures to a framebuffer, if
+    * this fails we'll try ARB_texture_rectangle.
+    */
+
+    GLenum textureTargets[] = {
+      LOCAL_GL_TEXTURE_2D,
+      LOCAL_GL_NONE
+    };
+
+    if (mGLContext->IsGLES2()) {
+        textureTargets[1] = LOCAL_GL_TEXTURE_RECTANGLE_ARB;
+    }
+
+    mFBOTextureTarget = LOCAL_GL_NONE;
+
+    for (PRUint32 i = 0; i < ArrayLength(textureTargets); i++) {
+      GLenum target = textureTargets[i];
+      if (!target)
+          continue;
+
+      mGLContext->fGenTextures(1, &mBackBufferTexture);
+      mGLContext->fBindTexture(target, mBackBufferTexture);
+      mGLContext->fTexParameteri(target,
+                                LOCAL_GL_TEXTURE_MIN_FILTER,
+                                LOCAL_GL_NEAREST);
+      mGLContext->fTexParameteri(target,
+                                LOCAL_GL_TEXTURE_MAG_FILTER,
+                                LOCAL_GL_NEAREST);
+      mGLContext->fTexImage2D(target,
+                              0,
+                              LOCAL_GL_RGBA,
+                              5, 3, /* sufficiently NPOT */
+                              0,
+                              LOCAL_GL_RGBA,
+                              LOCAL_GL_UNSIGNED_BYTE,
+                              NULL);
+
+      // unbind this texture, in preparation for binding it to the FBO
+      mGLContext->fBindTexture(target, 0);
+
+      mGLContext->fBindFramebuffer(LOCAL_GL_FRAMEBUFFER, mBackBufferFBO);
+      mGLContext->fFramebufferTexture2D(LOCAL_GL_FRAMEBUFFER,
+                                        LOCAL_GL_COLOR_ATTACHMENT0,
+                                        target,
+                                        mBackBufferTexture,
+                                        0);
+
+      if (mGLContext->fCheckFramebufferStatus(LOCAL_GL_FRAMEBUFFER) ==
+          LOCAL_GL_FRAMEBUFFER_COMPLETE)
+      {
+        mFBOTextureTarget = target;
+        break;
+      }
+
+      // We weren't succesful with this texture, so we don't need it
+      // any more.
+      mGLContext->fDeleteTextures(1, &mBackBufferTexture);
+    }
+
+    if (mFBOTextureTarget == LOCAL_GL_NONE) {
+      /* Unable to find a texture target that works with FBOs and NPOT textures */
+      return false;
+    }
+  } else {
+    // not trying to work around driver bugs, so TEXTURE_2D should just work
+    mFBOTextureTarget = LOCAL_GL_TEXTURE_2D;
+  }
+
+  // back to default framebuffer, to avoid confusion
+  mGLContext->fBindFramebuffer(LOCAL_GL_FRAMEBUFFER, 0);
+
+  if (mFBOTextureTarget == LOCAL_GL_TEXTURE_RECTANGLE_ARB) {
+    /* If we're using TEXTURE_RECTANGLE, then we must have the ARB
+     * extension -- the EXT variant does not provide support for
+     * texture rectangle access inside GLSL (sampler2DRect,
+     * texture2DRect).
+     */
+    if (!mGLContext->IsExtensionSupported(gl::GLContext::ARB_texture_rectangle))
+      return false;
+  }
+
+  // If we're double-buffered, we don't need this fbo anymore.
+  if (mGLContext->IsDoubleBuffered()) {
+    mGLContext->fDeleteFramebuffers(1, &mBackBufferFBO);
+    mBackBufferFBO = 0;
+  }
+
+  /* Create a simple quad VBO */
+
+  mGLContext->fGenBuffers(1, &mQuadVBO);
+  mGLContext->fBindBuffer(LOCAL_GL_ARRAY_BUFFER, mQuadVBO);
+
+  GLfloat vertices[] = {
+    /* First quad vertices */
+    0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+    /* Then quad texcoords */
+    0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+    /* Then flipped quad texcoords */
+    0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+  };
+  mGLContext->fBufferData(LOCAL_GL_ARRAY_BUFFER, sizeof(vertices), vertices, LOCAL_GL_STATIC_DRAW);
+  mGLContext->fBindBuffer(LOCAL_GL_ARRAY_BUFFER, 0);
+
+  nsCOMPtr<nsIConsoleService>
+    console(do_GetService(NS_CONSOLESERVICE_CONTRACTID));
+
+  if (console) {
+    nsString msg;
+    msg +=
+      NS_LITERAL_STRING("OpenGL LayerManager Initialized Succesfully.\nVersion: ");
+    msg += NS_ConvertUTF8toUTF16(
+      nsDependentCString((const char*)mGLContext->fGetString(LOCAL_GL_VERSION)));
+    msg += NS_LITERAL_STRING("\nVendor: ");
+    msg += NS_ConvertUTF8toUTF16(
+      nsDependentCString((const char*)mGLContext->fGetString(LOCAL_GL_VENDOR)));
+    msg += NS_LITERAL_STRING("\nRenderer: ");
+    msg += NS_ConvertUTF8toUTF16(
+      nsDependentCString((const char*)mGLContext->fGetString(LOCAL_GL_RENDERER)));
+    msg += NS_LITERAL_STRING("\nFBO Texture Target: ");
+    if (mFBOTextureTarget == LOCAL_GL_TEXTURE_2D)
+      msg += NS_LITERAL_STRING("TEXTURE_2D");
+    else
+      msg += NS_LITERAL_STRING("TEXTURE_RECTANGLE");
+    console->LogStringMessage(msg.get());
+  }
+
+  if (NS_IsMainThread()) {
+    Preferences::AddBoolVarCache(&sDrawFPS, "layers.acceleration.draw-fps");
+  } else {
+    // We have to dispatch an event to the main thread to read the pref.
+    class ReadDrawFPSPref : public nsRunnable {
+    public:
+      NS_IMETHOD Run()
+      {
+        Preferences::AddBoolVarCache(&sDrawFPS, "layers.acceleration.draw-fps");
+        return NS_OK;
+      }
+    };
+    NS_DispatchToMainThread(new ReadDrawFPSPref());
+  }
+
+  reporter.SetSuccessful();
+  return true;
+}
+
 
 TextureHostIdentifier
 CompositorOGL::GetTextureHostIdentifier()
@@ -39,7 +300,19 @@ CompositorOGL::DrawQuad(const gfx::Rect &aRect, const gfx::Rect *aSourceRect,
                         const gfx::Rect *aClipRect, const EffectChain &aEffectChain,
                         const gfx::Matrix3x3 &aTransform)
 {
-  // TODO: Implement this.
+  if (aEffectChain.mEffects[EFFECT_SOLID_COLOR]) {
+
+  } else if (aEffectChain.mEffects[EFFECT_BGRA]) {
+
+  } else if (aEffectChain.mEffects[EFFECT_BGRX]) {
+
+  } else if (aEffectChain.mEffects[EFFECT_YCBCR]) {
+
+  } else if (aEffectChain.mEffects[EFFECT_COMPONENT_ALPHA]) {
+
+  }
+
+  // TODO: Handle EFFECT_MAX.
 }
 
 } /* layers */
--- gfx/layers/opengl/CompositorOGL.h
+++ gfx/layers/opengl/CompositorOGL.h
@@ -7,6 +7,8 @@
 #define MOZILLA_GFX_COMPOSITOROGL_H
 
 #include "Compositor.h"
+#include "GLContext.h"
+#include "LayerManagerOGLProgram.h"
 
 namespace mozilla {
 
@@ -14,6 +16,23 @@ namespace layers {
 
 class CompositorOGL : public Compositor
 {
+  typedef mozilla::gl::GLContext GLContext;
+  typedef mozilla::gl::ShaderProgramType ProgramType;
+
+public:
+  CompositorOGL(nsIWidget *aWidget, int aSurfaceWidth = -1, int aSurfaceHeight = -1,
+                bool aIsRenderingToEGLSurface = false);
+
+  /**
+   * Initializes the compositor with a given GLContext. force should indicate
+   * whether GL layers have been force-enabled. If aContext is null, the compositor
+   * creates a context for the associated widget. Returns true if initialization
+   * is succesful, false otherwise.
+   */
+  bool Initialize(bool force = false, nsRefPtr<GLContext> aContext = nsnull);
+
+  void Destroy();
+
   virtual TextureHostIdentifier GetTextureHostIdentifier() MOZ_OVERRIDE;
 
   virtual TemporaryRef<Texture>
@@ -26,6 +45,93 @@ class CompositorOGL : public Compositor
   virtual void DrawQuad(const gfx::Rect &aRect, const gfx::Rect *aSourceRect,
                         const gfx::Rect *aClipRect, const EffectChain &aEffectChain,
                         const gfx::Matrix3x3 &aTransform) MOZ_OVERRIDE;
+
+  virtual PRInt32 GetMaxTextureSize() const
+  {
+    return mGLContext->GetMaxTextureSize();
+  }
+
+  /**
+   * Set the size of the EGL surface we're rendering to.
+   */
+  void SetSurfaceSize(int width, int height);
+
+  GLContext* gl() const { return mGLContext; }
+
+  void MakeCurrent(bool aForce = false) {
+    if (mDestroyed) {
+      NS_WARNING("Call on destroyed layer manager");
+      return;
+    }
+    mGLContext->MakeCurrent(aForce);
+  }
+
+private:
+  /** Widget associated with this compositor */
+  nsIWidget *mWidget;  // TODO: Do we really need to keep this?
+  nsRefPtr<GLContext> mGLContext;
+
+  /** The size of the surface we are rendering to */
+  nsIntSize mSurfaceSize;
+
+  already_AddRefed<mozilla::gl::GLContext> CreateContext();
+
+  /** Backbuffer */
+  GLuint mBackBufferFBO;
+  GLuint mBackBufferTexture;
+  nsIntSize mBackBufferSize;
+
+  /** Shader Programs */
+  struct ShaderProgramVariations {
+    ShaderProgramOGL* mVariations[NumMaskTypes];
+  };
+  nsTArray<ShaderProgramVariations> mPrograms;
+
+  /** Texture target to use for FBOs */
+  GLenum mFBOTextureTarget;
+
+  /** VBO that has some basics in it for a textured quad,
+   *  including vertex coords and texcoords for both
+   *  flipped and unflipped textures */
+  GLuint mQuadVBO;
+
+  bool mHasBGRA;
+
+  /**
+   * When rendering to an EGL surface (e.g. on Android), we rely on being told
+   * about size changes (via SetSurfaceSize) rather than pulling this information
+   * from the widget.
+   */
+  bool mIsRenderingToEGLSurface;
+
+  /**
+   * Helper method for Initialize, creates all valid variations of a program
+   * and adds them to mPrograms
+   */
+  void AddPrograms(gl::ShaderProgramType aType);
+
+  bool mDestroyed;
+
+  struct FPSState
+  {
+      GLuint texture;
+      int fps;
+      bool initialized;
+      int fcount;
+      TimeStamp last;
+
+      FPSState()
+        : texture(0)
+        , fps(0)
+        , initialized(false)
+        , fcount(0)
+      {
+        last = TimeStamp::Now();
+      }
+      void DrawFPS(GLContext*, ShaderProgramOGL*);
+  } mFPS;
+
+  static bool sDrawFPS;
 };
 
 }
