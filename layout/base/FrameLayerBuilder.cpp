4c497cf168fb90be002fe821849429d37235ebf6
Ali Juma <ajuma@mozilla.com>
2012-07-24 16:47:41 -0400

    Backed out changeset 743643a8ad64

--- layout/base/FrameLayerBuilder.cpp
+++ layout/base/FrameLayerBuilder.cpp
@@ -1553,7 +1553,7 @@ PaintInactiveLayer(nsDisplayListBuilder* aBuilder,
   nsRefPtr<BasicLayerManager> tempManager = new BasicLayerManager();
   tempManager->BeginTransactionWithTarget(context);
 #else
-  nsRefPtr<DirectLayerRenderer> tempManager = new DirectLayerRenderer(aContext->GetDrawTarget());
+  nsRefPtr<DirectLayerRenderer> tempManager;
 #endif
   nsRefPtr<Layer> layer =
     aItem->BuildLayer(aBuilder, tempManager, FrameLayerBuilder::ContainerParameters());
